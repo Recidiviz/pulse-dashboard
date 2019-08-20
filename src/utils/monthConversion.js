75958c2314bc5f03492f56c6ad50dae3f75e0a6a
Julia Dressel <julia@recidiviz.org>
2019-08-20 13:56:41 -0700

    Updating logic of adding years

--- src/utils/monthConversion.js
+++ src/utils/monthConversion.js
@@ -30,21 +30,44 @@ const monthNamesShortWithYearsFromNumberList = function monthNamesShortWithYears
   numberList,
 ) {
   const monthNames = monthNamesShortFromNumberList(numberList);
+  const multipleYears = (numberList.length > 12
+    || numberList[numberList.length - 1] < numberList[0]);
 
-  if (numberList.length > 12 || numberList[numberList.length - 1] < numberList[0]) {
-    const today = new Date();
-    let year = today.getFullYear();
+  const today = new Date();
+  let year = today.getFullYear();
 
-    for (let i = numberList.length - 1; i >= 0; i -= 1) {
-      if (numberList[i] === 1) {
-        monthNames[i] = monthNames[i].concat(" '", year % 100);
-        year -= 1;
-      } else if (i === 0) {
-        monthNames[i] = monthNames[i].concat(" '", year % 100);
-      }
+  for (let i = numberList.length - 1; i >= 0; i -= 1) {
+    if (i === 0) {
+      monthNames[i] = monthNames[i].concat(" '", year % 100);
+    } else if (multipleYears && numberList[i] === 1) {
+      monthNames[i] = monthNames[i].concat(" '", year % 100);
+      year -= 1;
     }
   }
   return monthNames;
+};
+
+const addYearsToMonthNamesShort = function addYearsToMonthNamesShort(
+  monthList,
+) {
+  const newMonthList = monthList;
+
+  const indexLastMonth = MONTH_NAMES_SHORT.indexOf(monthList[monthList.length - 1]);
+  const indexFirstMonth = MONTH_NAMES_SHORT.indexOf(monthList[0]);
+  const multipleYears = (monthList.length > 12 || indexLastMonth < indexFirstMonth);
+
+  const today = new Date();
+  let year = today.getFullYear();
+
+  for (let i = monthList.length - 1; i >= 0; i -= 1) {
+    if (i === 0) {
+      newMonthList[i] = monthList[i].concat(" '", year % 100);
+    } else if (multipleYears && monthList[i] === 'Jan') {
+      newMonthList[i] = monthList[i].concat(" '", year % 100);
+      year -= 1;
+    }
+  }
+  return newMonthList;
 };
 
 const monthNamesFromShortName = function monthNamesFromShortName(shortName) {
@@ -52,6 +75,7 @@ const monthNamesFromShortName = function monthNamesFromShortName(shortName) {
 };
 
 export {
+  addYearsToMonthNamesShort,
   monthNameFromNumber,
   monthNameShortFromNumber,
   monthNamesShortFromNumberList,
