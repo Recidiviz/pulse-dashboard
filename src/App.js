335285b1329453c572eaa0f01066a936e8db84a9
Joshua Essex <essexjosh@gmail.com>
2020-03-11 10:28:11 -0600

    DO NOT MERGE: Testing the Travis build.

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@
 // =============================================================================
 
 import React, { useEffect, useState } from 'react';
+
 import {
   BrowserRouter as Router, Redirect, Route, Switch,
 } from 'react-router-dom';
