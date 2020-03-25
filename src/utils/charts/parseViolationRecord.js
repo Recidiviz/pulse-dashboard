74305283485900a3efd2ef4844c8be737e05fd6e
Ian MacFarland <ian@recidiviz.org>
2020-03-24 17:23:33 -0700

    remove array.indexOf reimplementation

--- src/utils/charts/parseViolationRecord.js
+++ src/utils/charts/parseViolationRecord.js
@@ -2,15 +2,6 @@ const VIOLATION_SEVERITY = [
   'fel', 'misd', 'absc', 'muni', 'subs', 'tech',
 ];
 
-const indexOf = (element, array) => {
-  for (let i = 0; i < array.length; i += 1) {
-    if (element === array[i]) {
-      return i;
-    }
-  }
-  return -1;
-};
-
 const parseViolationRecord = (recordLabel) => {
   if (!recordLabel) {
     return '';
@@ -19,8 +10,8 @@ const parseViolationRecord = (recordLabel) => {
   const recordParts = recordLabel.split(';');
   const recordPartRegex = /(?<number>\d+)(?<abbreviation>\w+)/;
   const records = recordParts.map((recordPart) => recordPart.match(recordPartRegex).groups);
-  records.sort((a, b) => indexOf(a.abbreviation, VIOLATION_SEVERITY)
-    - indexOf(b.abbreviation, VIOLATION_SEVERITY));
+  records.sort((a, b) => VIOLATION_SEVERITY.indexOf(a.abbreviation)
+    - VIOLATION_SEVERITY.indexOf(b.abbreviation));
 
   return records.map((record) => `${record.number} ${record.abbreviation}`).join(', ');
 };
