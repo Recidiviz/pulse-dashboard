ccf4af1a1857e5aa2ab6da7a81a8bf1215e26579
Jen <jovergaag@gmail.com>
2020-10-05 18:35:44 -0700

    cleanup

--- src/views/tenants/us_mo/Layout.js
+++ src/views/tenants/us_mo/Layout.js
@@ -23,9 +23,10 @@ import TopBarLayout from "../../../components/topbar/TopBarLayout";
 import TopBarLogo from "../../../components/topbar/TopBarLogo";
 import TopBarUserMenuForAuthenticatedUser from "../../../components/topbar/TopBarUserMenuForAuthenticatedUser";
 import { setTranslateLocale } from "../utils/i18nSettings";
+import * as lanternTenant from "../utils/lanternTenants";
 
 const Layout = ({ children }) => {
-  setTranslateLocale("us_mo");
+  setTranslateLocale(lanternTenant.MO);
 
   return (
     <div id="app">
