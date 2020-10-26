28a3a90cd2e5c67b4f427982692127489758298e
Vladislav.Kirilenok <vladislav.kirilenok@gmail.com>
2020-10-26 15:57:01 +0300

    App tests

--- src/App.test.js
+++ src/App.test.js
@@ -15,18 +15,106 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 // =============================================================================
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-// import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
 
-// TODO: Currently fails because it's not picking up all dependencies,
-// namely jQuery (which should go away) and Auth0
-// it('renders without crashing', () => {
-//   const div = document.createElement('div');
-//   ReactDOM.render(<App />, div);
-//   ReactDOM.unmountComponentAtNode(div);
-// });
+import App from "./App";
+import { useAuth0 } from "./react-auth0-spa";
+import { ND } from "./views/tenants/utils/coreTenants";
+import { MO, PA } from "./views/tenants/utils/lanternTenants";
+import UsMoCommunityRevocations from "./views/tenants/us_mo/community/Revocations";
+import UsPaCommunityRevocations from "./views/tenants/us_pa/community/Revocations";
+import UsNdCommunityGoals from "./views/tenants/us_nd/community/Goals";
+import NotFound from "./views/NotFound";
 
-it('does nothing for now', () => {
+jest.mock("./utils/intercomSettings");
+jest.mock("./utils/initFontAwesome");
+jest.mock("./views/tenants/utils/i18nSettings");
+jest.mock("./views/tenants/us_mo/community/Revocations");
+jest.mock("./views/tenants/us_pa/community/Revocations");
+jest.mock("./views/tenants/us_nd/community/Goals");
+jest.mock("./views/NotFound");
+jest.mock("./react-auth0-spa");
 
+describe("App tests", () => {
+  UsMoCommunityRevocations.mockReturnValue(null);
+  UsPaCommunityRevocations.mockReturnValue(null);
+  UsNdCommunityGoals.mockReturnValue(null);
+  NotFound.mockReturnValue(null);
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should render MO revocations page", () => {
+    window.history.pushState(
+      {},
+      "MO revocations page",
+      "/community/revocations"
+    );
+    const user = {
+      "https://dashboard.recidiviz.org/app_metadata": {
+        state_code: MO,
+      },
+    };
+    useAuth0.mockReturnValue({
+      user,
+      isAuthenticated: true,
+      loading: false,
+      loginWithRedirect: jest.fn(),
+      getTokenSilently: jest.fn(),
+    });
+    render(<App />);
+
+    expect(UsMoCommunityRevocations).toHaveBeenCalled();
+  });
+
+  it("should render ND community goals page", () => {
+    window.history.pushState({}, "ND community goals page", "/community/goals");
+    const user = {
+      "https://dashboard.recidiviz.org/app_metadata": {
+        state_code: ND,
+      },
+    };
+    useAuth0.mockReturnValue({
+      user,
+      isAuthenticated: true,
+      loading: false,
+      loginWithRedirect: jest.fn(),
+      getTokenSilently: jest.fn(),
+    });
+    render(<App />);
+
+    expect(UsNdCommunityGoals).toHaveBeenCalled();
+  });
+
+  it("should render PA community goals page", () => {
+    window.history.pushState(
+      {},
+      "PA revocations page",
+      "/community/revocations"
+    );
+    const user = {
+      "https://dashboard.recidiviz.org/app_metadata": {
+        state_code: PA,
+      },
+    };
+    useAuth0.mockReturnValue({
+      user,
+      isAuthenticated: true,
+      loading: false,
+      loginWithRedirect: jest.fn(),
+      getTokenSilently: jest.fn(),
+    });
+    render(<App />);
+
+    expect(UsPaCommunityRevocations).toHaveBeenCalled();
+  });
+
+  it("should render Not Found page ", () => {
+    window.history.pushState({}, "PA revocations page", "/some/page");
+    render(<App />);
+
+    expect(NotFound).toHaveBeenCalled();
+  });
 });
