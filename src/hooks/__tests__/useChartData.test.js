e58d8dfaa2653bc16d4f6d6290f8ee50f4796ef2
Vladislav.Kirilenok <vladislav.kirilenok@gmail.com>
2020-11-06 18:45:19 +0300

    tests simplified

--- src/hooks/__tests__/useChartData.test.js
+++ src/hooks/__tests__/useChartData.test.js
@@ -14,61 +14,44 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 // =============================================================================
-import React from "react";
-import { render } from "@testing-library/react";
+import { renderHook } from "@testing-library/react-hooks";
 
 import useChartData from "../useChartData";
-import Error from "../../components/Error";
 import { callMetricsApi } from "../../api/metrics/metricsClient";
 import { useAuth0 } from "../../react-auth0-spa";
 
 jest.mock("../../react-auth0-spa");
 jest.mock("../../api/metrics/metricsClient");
-
-const TestComponent = () => {
-  const { apiData, isError } = useChartData("anyURL", "anyFile");
-
-  return (
-    <>
-      <div data-testid="apiData">{apiData}</div>
-      {isError && <div>Error</div>}
-    </>
-  );
-};
-
 describe("useChartData", () => {
   describe("when an error is thrown", () => {
-    beforeEach(() => {
-      callMetricsApi.mockImplementation(() => {
-        throw new Error();
-      });
-
-      useAuth0.mockReturnValue({
-        user: {},
-        isAuthenticated: true,
-        loading: true,
-        loginWithRedirect: jest.fn(),
-        getTokenSilently: jest.fn(),
-      });
-
-      // do not log the expected error - keep tests less verbose
-      jest.spyOn(console, "error").mockImplementation(() => {});
+    callMetricsApi.mockImplementation(() => {
+      throw new Error();
     });
+
+    useAuth0.mockReturnValue({
+      user: {},
+      isAuthenticated: true,
+      loading: true,
+      loginWithRedirect: jest.fn(),
+      getTokenSilently: jest.fn(),
+    });
+
+    // do not log the expected error - keep tests less verbose
+    jest.spyOn(console, "error").mockImplementation(() => {});
 
     afterEach(() => {
       jest.clearAllMocks();
     });
 
-    it("returns an empty array for apiData", () => {
-      const { getByTestId } = render(<TestComponent />);
+    it("returns isError = true", async () => {
+      const { result, waitForNextUpdate } = renderHook(() =>
+        useChartData("anyURL", "anyFile")
+      );
 
-      expect(getByTestId("apiData")).toHaveTextContent([]);
-    });
+      await waitForNextUpdate();
 
-    it("returns isError = true", () => {
-      const { getByText } = render(<TestComponent />);
-
-      expect(getByText("Error")).toBeTruthy();
+      expect(result.current.isError).toBe(true);
+      expect(result.current.apiData).toEqual([]);
     });
   });
 });
