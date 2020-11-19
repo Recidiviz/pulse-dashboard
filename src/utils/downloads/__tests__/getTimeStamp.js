3e965973fabdd092f5d0850ba471702eea173902
Vladislav.Kirilenok <vladislav.kirilenok@gmail.com>
2020-11-19 18:39:13 +0300

    fixed timezone tests issues

--- src/utils/downloads/__tests__/getTimeStamp.js
+++ src/utils/downloads/__tests__/getTimeStamp.js
@@ -4,7 +4,7 @@ describe("getTimeStamp tests", () => {
   const dateNowSpy = jest.spyOn(Date, "now");
   dateNowSpy.mockReturnValue(1605792733106);
 
-  it("sd", () => {
-    expect(getTimeStamp()).toBe("11-19-2020-04-32-13-PM");
+  it("should format to proper format", () => {
+    expect(getTimeStamp()).toBe("11-19-2020-01-32-13-PM");
   });
 });
