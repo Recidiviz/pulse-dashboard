f9c56d0120ca4ea0801b151dfcd89f65c420521d
Daniela Schiano di Cola <daniela@recidiviz.org>
2021-01-06 19:37:20 -0500

    WIP

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 // =============================================================================
 
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import {
   Redirect,
   Route,
@@ -26,25 +26,44 @@ import {
 import TenantRoutes from "./components/TenantRoutes";
 import { PageProvider } from "./contexts/PageContext";
 import NotFound from "./views/NotFound";
-import Profile from "./views/Profile";
+// import Profile from "./views/Profile";
 import VerificationNeeded from "./views/VerificationNeeded";
 import LanternLayout from "./components/layouts/LanternLayout";
 import CoreLayout from "./components/layouts/CoreLayout";
-import Revocations from "./components/Revocations";
-import UsNdCommunityGoals from "./views/tenants/us_nd/community/Goals";
-import UsNdCommunityExplore from "./views/tenants/us_nd/community/Explore";
-import UsNdFacilitiesGoals from "./views/tenants/us_nd/facilities/Goals";
-import UsNdFacilitiesExplore from "./views/tenants/us_nd/facilities/Explore";
-import UsNdProgrammingExplore from "./views/tenants/us_nd/programming/Explore";
+// import Revocations from "./components/Revocations";
+// import UsNdCommunityGoals from "./views/tenants/us_nd/community/Goals";
+// import UsNdCommunityExplore from "./views/tenants/us_nd/community/Explore";
+// import UsNdFacilitiesGoals from "./views/tenants/us_nd/facilities/Goals";
+// import UsNdFacilitiesExplore from "./views/tenants/us_nd/facilities/Explore";
+// import UsNdProgrammingExplore from "./views/tenants/us_nd/programming/Explore";
 import initFontAwesome from "./utils/initFontAwesome";
 import initIntercomSettings from "./utils/initIntercomSettings";
 import { initI18n } from "./views/tenants/utils/i18nSettings";
 import { LANTERN_TENANTS } from "./views/tenants/utils/lanternTenants";
 import { CORE_TENANTS } from "./views/tenants/utils/coreTenants";
 import StoreProvider from "./StoreProvider";
+import Loading from "./components/Loading";
 
 import "./assets/scripts/index";
 import "./assets/styles/index.scss";
+
+const Revocations = lazy(() => import("./components/Revocations"));
+const UsNdCommunityGoals = lazy(() =>
+  import("./views/tenants/us_nd/community/Goals")
+);
+const UsNdCommunityExplore = lazy(() =>
+  import("./views/tenants/us_nd/community/Explore")
+);
+const UsNdFacilitiesGoals = lazy(() =>
+  import("./views/tenants/us_nd/facilities/Goals")
+);
+const UsNdFacilitiesExplore = lazy(() =>
+  import("./views/tenants/us_nd/facilities/Explore")
+);
+const UsNdProgrammingExplore = lazy(() =>
+  import("./views/tenants/us_nd/programming/Explore")
+);
+const Profile = lazy(() => import("./views/Profile"));
 
 initFontAwesome();
 initIntercomSettings();
@@ -61,22 +80,26 @@ const App = () => (
           <TenantRoutes>
             <LanternLayout tenantIds={LANTERN_TENANTS}>
               <Switch>
-                <Route path="/community/revocations" component={Revocations} />
-                <Route path="/profile" component={Profile} />
-                <Redirect exact from="/" to="/community/revocations" />
-                <Redirect from="/revocations" to="/community/revocations" />
-                <NotFound />
+                <Suspense fallback={<Loading />}>
+                  <Route path="/community/revocations" component={Revocations} />
+                  <Route path="/profile" component={Profile} />
+                </Suspense>
+                  <Redirect exact from="/" to="/community/revocations" />
+                  <Redirect from="/revocations" to="/community/revocations" />
+                  <NotFound />
               </Switch>
             </LanternLayout>
 
             <CoreLayout tenantIds={CORE_TENANTS}>
               <Switch>
-                <Route path="/community/goals" component={UsNdCommunityGoals} />
-                <Route path="/community/explore" component={UsNdCommunityExplore} />
-                <Route path="/facilities/goals" component={UsNdFacilitiesGoals} />
-                <Route path="/facilities/explore" component={UsNdFacilitiesExplore} />
-                <Route path="/programming/explore" component={UsNdProgrammingExplore} />
-                <Route path="/profile" component={Profile} />
+                <Suspense fallback={<Loading />}>
+                  <Route path="/community/goals" component={UsNdCommunityGoals} />
+                  <Route path="/community/explore" component={UsNdCommunityExplore} />
+                  <Route path="/facilities/goals" component={UsNdFacilitiesGoals} />
+                  <Route path="/facilities/explore" component={UsNdFacilitiesExplore} />
+                  <Route path="/programming/explore" component={UsNdProgrammingExplore} />
+                  <Route path="/profile" component={Profile} />
+                </Suspense>
                 <Redirect exact from="/" to="/community/goals" />
                 <Redirect from="/snapshots" to="/community/goals" />
                 <Redirect from="/revocations" to="/community/goals" />
