e8b585089d08590418e5e8c64d65f546c3ae89f9
Jen <jovergaag@gmail.com>
2021-02-16 13:36:30 -0800

    rename Sortable.css to Sortable.scss

--- src/components/charts/new_revocations/CaseTable/Sortable.js
+++ src/components/charts/new_revocations/CaseTable/Sortable.js
@@ -17,7 +17,7 @@
 
 import React from "react";
 import PropTypes from "prop-types";
-import "./Sortable.css";
+import "./Sortable.scss";
 
 function SortableIcon({ order = null }) {
   switch (order) {
