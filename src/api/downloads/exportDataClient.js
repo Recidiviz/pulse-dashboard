78d8e70bdb37a3a89eb7e14393a28df6acb4a8b7
Daniela Schiano di Cola <daniela@recidiviz.org>
2021-03-01 18:07:45 -0500

    Add an exportDataClient in the api directory

---
+++ src/api/downloads/exportDataClient.js
@@ -0,0 +1,48 @@
+// Recidiviz - a data platform for criminal justice reform
+// Copyright (C) 2021 Recidiviz, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+// =============================================================================
+import JsFileDownloader from "js-file-downloader";
+
+export default async function exportDataClient(
+  formData,
+  filename,
+  getTokenSilently
+) {
+  const token = await getTokenSilently();
+
+  return fetch(`${process.env.REACT_APP_API_URL}/api/generateFileLink`, {
+    method: "POST",
+    body: formData,
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  })
+    .then((response) => response.text())
+    .then((url) => {
+      // This file downloader should not be used for mobile Safari.
+      // The download for mobile Safari is handled in downloadZipFile.
+      const jsFileDownload = new JsFileDownloader({
+        autoStart: false,
+        filename,
+        url,
+      });
+      jsFileDownload.start();
+    })
+    .catch((error) => {
+      /* eslint-disable-next-line no-console */
+      console.error(error.message);
+    });
+}
