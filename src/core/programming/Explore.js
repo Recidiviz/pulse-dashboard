32b8aae7d4fd88994b0f6a56e6f3bdd2495fd109
Ivan Bobchenkov <44039577+MeloncholyShiro@users.noreply.github.com>
2021-03-08 09:31:45 +0300

    Added Toggle Pill (#822)
    * added toggle pill
    
    * style fix + split options props
    
    * 2019 -> 2021
    
    * Rebase, modify TogglePill to take its current value as a prop, and add tests
    
    * Fix style names
    
    Co-authored-by: Ivan Bobchenkov <IOBobchenkov1@sberbank.ru>
    Co-authored-by: Anthony Kesich <anthony@recidiviz.org>

--- src/core/programming/Explore.js
+++ src/core/programming/Explore.js
@@ -58,6 +58,7 @@ const ProgrammingExplore = () => {
 
   const filters = (
     <FiltersBar
+      metricType={metricType}
       metricPeriodMonths={metricPeriodMonths}
       district={district}
       supervisionType={supervisionType}
