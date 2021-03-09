a166249c54b8631605127b7b9245f5e0024ccd13
Alexander Kozlovskiy <a.kozlovskiy@dunice.net>
2021-03-09 10:27:24 +0300

    added listenner remover

--- src/components/methodology/Methodology.js
+++ src/components/methodology/Methodology.js
@@ -32,7 +32,15 @@ const Methodology = () => {
         const blockID = anchor.getAttribute("href").substr(1);
         document.getElementById(blockID).scrollIntoView({
           behavior: "smooth",
-          block: "end",
+          block: "start",
+        });
+      });
+      return anchor.removeEventListener("click", (e) => {
+        e.preventDefault();
+        const blockID = anchor.getAttribute("href").substr(1);
+        document.getElementById(blockID).scrollIntoView({
+          behavior: "smooth",
+          block: "start",
         });
       });
     });
