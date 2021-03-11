c3de1a51dc86c8fd22a9086a87e7033a6fba98da
Alexander Kozlovskiy <a.kozlovskiy@dunice.net>
2021-03-11 18:22:25 +0300

    added mock card content

--- src/core/community/constants.js
+++ src/core/community/constants.js
@@ -57,3 +57,36 @@ export const metrics = {
   metricPeriodMonths: "36",
   supervisionType: "all",
 };
+
+export const mockCardContent = [
+  {
+    title: "Overall",
+    percent: 79,
+    status: "NEEDS_IMPROVEMENT",
+    id: 1,
+  },
+  {
+    title: "Program availability",
+    percent: 56,
+    status: "POOR",
+    id: 2,
+  },
+  {
+    title: "Timely contacts",
+    percent: 100,
+    status: "GREAT",
+    id: 3,
+  },
+  {
+    title: "Timely risk assessments",
+    percent: 95,
+    status: "GREAT",
+    id: 4,
+  },
+  {
+    title: "Timely discharge",
+    percent: 79,
+    status: "GOOD",
+    id: 5,
+  },
+];
