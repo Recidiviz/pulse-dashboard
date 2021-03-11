98c09562ce9ad2513c22c4b11e14bae997da638f
Daniela Schiano di Cola <daniela@recidiviz.org>
2021-03-11 14:06:07 -0500

    Add case triage

--- src/App.js
+++ src/App.js
@@ -45,8 +45,6 @@ import { LANTERN_TENANTS } from "./RootStore/TenantStore/lanternTenants";
 import { CORE_TENANTS } from "./RootStore/TenantStore/coreTenants";
 import AuthWall from "./AuthWall";
 import ErrorBoundary from "./components/ErrorBoundary";
-import { CoreLoading } from "./components/CoreLoadingIndicator";
-import PageTemplate from "./core/PageTemplate";
 
 import "./assets/scripts/index";
 import "./assets/styles/index.scss";
