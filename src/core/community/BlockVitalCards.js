02c7fce3808a5b6a95a507c893fcd821205888f1
Alexander Kozlovskiy <a.kozlovskiy@dunice.net>
2021-03-12 12:50:12 +0300

    removed sorting and refactor styles

--- src/core/community/BlockVitalCards.js
+++ src/core/community/BlockVitalCards.js
@@ -12,25 +12,16 @@ const BlockVitalCards = () => {
   const handlerSelectedCards = (id) => {
     setSelectCard(id);
   };
-  const sortFunction = (a, b) => {
-    if (a.percent > b.percent) {
-      return 1;
-    }
-    if (a.percent < b.percent) {
-      return -1;
-    }
-    return 0;
-  };
   const groupingCard = groupBy(mockCardContent, "status");
   const orderedCard = [
-    ...get(groupingCard, "NEEDS_IMPROVEMENT", []).sort(sortFunction),
-    ...get(groupingCard, "POOR", []).sort(sortFunction),
-    ...get(groupingCard, "GOOD", []).sort(sortFunction),
-    ...get(groupingCard, "GREAT", []).sort(sortFunction),
+    ...get(groupingCard, "NEEDS_IMPROVEMENT", []),
+    ...get(groupingCard, "POOR", []),
+    ...get(groupingCard, "GOOD", []),
+    ...get(groupingCard, "GREAT", []),
   ];
 
   return (
-    <div className="CoreCommunityVitals__card-container col-12 row">
+    <div className="CoreCommunityVitals__card-container row">
       {orderedCard.map(({ title, percent, status, id }) => (
         <CoreVitalSummaryCard
           key={id}
