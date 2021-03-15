38235c25bd0484da9cc2589b5f014a3efcd97a27
Alexander Kozlovskiy <a.kozlovskiy@dunice.net>
2021-03-15 15:21:34 +0300

    removed the router and redirect to programming/explore

--- src/App.js
+++ src/App.js
@@ -36,7 +36,6 @@ import UsNdCommunityGoals from "./core/community/Goals";
 import UsNdCommunityExplore from "./core/community/Explore";
 import UsNdFacilitiesGoals from "./core/facilities/Goals";
 import UsNdFacilitiesExplore from "./core/facilities/Explore";
-import UsNdProgrammingExplore from "./core/programming/Explore";
 import initFontAwesome from "./utils/initFontAwesome";
 import initIntercomSettings from "./utils/initIntercomSettings";
 import { initI18n } from "./utils/i18nSettings";
@@ -82,13 +81,11 @@ const App = () => (
                     <Route path="/community/explore" component={UsNdCommunityExplore} />
                     <Route path="/facilities/goals" component={UsNdFacilitiesGoals} />
                     <Route path="/facilities/explore" component={UsNdFacilitiesExplore} />
-                    <Route path="/programming/explore" component={UsNdProgrammingExplore} />
                     <Route path="/profile" component={Profile} />
                     <Redirect exact from="/" to="/community/goals" />
                     <Redirect from="/snapshots" to="/community/goals" />
                     <Redirect from="/revocations" to="/community/goals" />
                     <Redirect from="/reincarcerations" to="/facilities/goals" />
-                    <Redirect from="/programEvaluation/freeThroughRecovery" to="/programming/explore" />
                     <NotFound />
                   </Switch>
                 </CoreLayout>
