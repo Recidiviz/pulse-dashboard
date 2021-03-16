2f61595c810eb52ecc6c54100422cec0e82ae503
Daniela Schiano di Cola <daniela@recidiviz.org>
2021-03-16 11:17:49 -0400

    Move isLoading to SummaryMetrics

--- src/core/PopulationSummaryMetrics/ProjectedSummaryMetrics.tsx
+++ src/core/PopulationSummaryMetrics/ProjectedSummaryMetrics.tsx
@@ -21,41 +21,54 @@ import LoadingMetrics from "./LoadingMetrics";
 import type { ProjectedSummaryRecord } from "../models/types";
 import "./PopulationSummaryMetrics.scss";
 
-const SummaryMetrics: React.FC<{ data: ProjectedSummaryRecord }> = ({
-  data,
-}) => (
+const SummaryMetrics: React.FC<{
+  isLoading: boolean;
+  data?: ProjectedSummaryRecord;
+}> = ({ isLoading, data }) => (
   <div className="SummaryMetrics">
-    <SummaryMetric
-      title="New arrivals"
-      value={data.admissionCount}
-      percentChange={data.admissionPercentChange}
-      deltaDirection={getDeltaDirection({
-        percentChange: data.admissionPercentChange,
-      })}
-      projectedMinMax={[data.admissionCountMin, data.admissionCountMax]}
-    />
-    <SummaryMetric
-      title="Releases"
-      value={data.releaseCount}
-      percentChange={data.releasePercentChange}
-      deltaDirection={getDeltaDirection({
-        percentChange: data.releasePercentChange,
-        improvesOnIncrease: true,
-      })}
-      projectedMinMax={[data.releaseCountMin, data.releaseCountMax]}
-    />
-    <SummaryMetric
-      title="Total population"
-      value={data.totalPopulation}
-      percentChange={data.populationPercentChange}
-      deltaDirection={getDeltaDirection({
-        percentChange: data.populationPercentChange,
-      })}
-      projectedMinMax={[
-        data.totalPopulationCountMin,
-        data.totalPopulationCountMax,
-      ]}
-    />
+    {isLoading ? (
+      <>
+        <LoadingMetrics title="New arrivals" showMinMax />
+        <LoadingMetrics title="Releases" showMinMax />
+        <LoadingMetrics title="Total population" showMinMax />
+      </>
+    ) : (
+      data && (
+        <>
+          <SummaryMetric
+            title="New arrivals"
+            value={data.admissionCount}
+            percentChange={data.admissionPercentChange}
+            deltaDirection={getDeltaDirection({
+              percentChange: data.admissionPercentChange,
+            })}
+            projectedMinMax={[data.admissionCountMin, data.admissionCountMax]}
+          />
+          <SummaryMetric
+            title="Releases"
+            value={data.releaseCount}
+            percentChange={data.releasePercentChange}
+            deltaDirection={getDeltaDirection({
+              percentChange: data.releasePercentChange,
+              improvesOnIncrease: true,
+            })}
+            projectedMinMax={[data.releaseCountMin, data.releaseCountMax]}
+          />
+          <SummaryMetric
+            title="Total population"
+            value={data.totalPopulation}
+            percentChange={data.populationPercentChange}
+            deltaDirection={getDeltaDirection({
+              percentChange: data.populationPercentChange,
+            })}
+            projectedMinMax={[
+              data.totalPopulationCountMin,
+              data.totalPopulationCountMax,
+            ]}
+          />
+        </>
+      )
+    )}
   </div>
 );
 
@@ -63,27 +76,15 @@ const ProjectedSummaryMetrics: React.FC<{
   data?: ProjectedSummaryRecord;
   isLoading: boolean;
 }> = ({ data, isLoading }) => {
-  if (isLoading) {
-    return (
-      <MetricsCard heading="Next 6 months" subheading="Projected">
-        <div className="SummaryMetrics">
-          <LoadingMetrics title="New arrivals" showMinMax />
-          <LoadingMetrics title="Releases" showMinMax />
-          <LoadingMetrics title="Total population" showMinMax />
-        </div>
-      </MetricsCard>
-    );
-  }
-
   return (
     <MetricsCard heading="Next 6 months" subheading="Projected">
-      {!data ? (
+      {!data && !isLoading ? (
         <div className="MissingProjectionData">
           There are not enough data to generate a projection for this subset of
           the population.
         </div>
       ) : (
-        <SummaryMetrics data={data} />
+        <SummaryMetrics isLoading={isLoading} data={data} />
       )}
     </MetricsCard>
   );
