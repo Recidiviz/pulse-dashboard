da075761d690d0d3842c7f59697b691dd67d34f4
Daniela Schiano di Cola <daniela@recidiviz.org>
2021-03-23 11:42:33 -0400

    Add react-table types config

--- src/core/VitalsSummaryTable/VitalsSummaryTable.tsx
+++ src/core/VitalsSummaryTable/VitalsSummaryTable.tsx
@@ -14,7 +14,6 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 // =============================================================================
-// @ts-nocheck
 import React, { useMemo } from "react";
 import { useTable, useSortBy } from "react-table";
 import cx from "classnames";
